3808303b4f85904ee492124004e2eabf667bc4b1
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-25 17:34:20 -0300

    feat: ExamController

---
+++ api/controllers/ExamController.js
@@ -0,0 +1,27 @@
+const database = require("../models");
+const Validation = require("../utils/Validation");
+const Helper = require("../utils/Helper");
+
+class ExamController {
+  static async validateAnomaly(req, res) {
+    try {
+      const matrix = Validation?.treatMatrixParameter(req?.matrix);
+
+      const checkAnomalies = Helper?.checkAnomalies();
+      if (
+        checkAnomalies?.diagonalOccurrences > 0 ||
+        checkAnomalies?.verticalOccurrences > 0 ||
+        checkAnomalies?.horizontalOccurrences > 0
+      ) {
+        return res?.status(200)?.send({ message: "Anomalies were found." });
+      } else {
+        return res?.status(403)?.send({ message: "No anomalies were found." });
+      }
+    } catch (error) {
+      const { message, status } = error;
+      return res?.status(status)?.send({ message });
+    }
+  }
+}
+
+module.exports = ExamController;
