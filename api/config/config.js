dadcf7b91d9419a6f7ad9cfcf91c61477d6304bf
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-25 13:26:44 -0300

    feat: prettier and dotenv

---
+++ api/config/config.js
@@ -0,0 +1,27 @@
+require("dotenv/config");
+
+const config = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
+
+return config;
--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app?.use(bodyParser.json())
+app?.use(bodyParser.json());
 
-const port = 3000
+const port = 3000;
 
-app?.get('/stats', (req, res) => res
-  ?.status(200)
-  ?.send({
+app?.get("/stats", (req, res) =>
+  res?.status(200)?.send({
     count_anomalies: 30,
     count_no_anomalies: 70,
     ratio: 70,
-}))
+  }),
+);
 
-app?.listen(port, () => console.log(`Server running on port ${port}`))
+app?.listen(port, () => console.log(`Server running on port ${port}`));
 
-module.exports = app
+module.exports = app;
