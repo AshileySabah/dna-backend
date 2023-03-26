720fac511ee436873486cb756a489bfc2ab12277
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-26 16:13:54 -0300

    test cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("./config/cors");
 
 const app = express();
 routes(app);
+
+app?.use(cors);
 
 const port = process.env.DATABASE_PORT;
 app?.listen(port, () => console.log(`Server running on port ${port}`));
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,19 +1,8 @@
 const bodyParser = require("body-parser");
 const examRoutes = require("./examRoutes");
-const cors = require("cors");
 
-module.exports = (app) => {
+module.exports = async (app) => {
   app?.use(bodyParser.json());
-  // app.use(cors());
-
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept",
-    );
-    next();
-  });
 
   app?.get("/", (req, res) =>
     res?.status(200)?.send({ message: "Welcome to DNA service" }),
