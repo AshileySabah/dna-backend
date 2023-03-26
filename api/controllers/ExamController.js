235962a39476493f210a35ecd57df2716c814d55
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-26 17:25:23 -0300

    fix: post anomalies

--- api/controllers/ExamController.js
+++ api/controllers/ExamController.js
@@ -8,8 +8,8 @@ class ExamController {
       const matrix = Validation?.treatMatrixParameter(req?.body?.matrix);
 
       const count_anomalies = Helper?.checkAnomalies(matrix)?.allOccurrences;
+      await database?.Exams?.create({ count_anomalies });
       if (count_anomalies > 0) {
-        await database?.Exams?.create({ count_anomalies });
         return res?.status(200)?.send({ message: "Anomalies were found." });
       } else {
         return res?.status(403)?.send({ message: "No anomalies were found." });
