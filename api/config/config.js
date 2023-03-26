bdc150fff552caf8bbe08b9577776ea419f999e1
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-26 12:21:59 -0300

    feat: database env config

--- api/config/config.js
+++ api/config/config.js
@@ -1,25 +1,15 @@
 require("dotenv/config");
 
+const databaseConfig = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: "mysql",
+};
+
 module.exports = {
-  development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    dialect: "mysql",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+  development: databaseConfig,
+  test: databaseConfig,
+  production: databaseConfig,
 };
