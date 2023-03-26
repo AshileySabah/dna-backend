1e582c68d7fc2fafcf9d655a9004fb1a2afd31c2
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-26 17:06:14 -0300

    feat: remove cors dependency

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,8 @@
 const bodyParser = require("body-parser");
 const examRoutes = require("./examRoutes");
-const cors = require("cors");
 
 module.exports = async (app) => {
   app?.use(bodyParser.json());
-  // app?.use(cors());
-
-  app?.get("/", (req, res) =>
-    res?.status(200)?.send({ message: "Welcome to DNA service" }),
-  );
 
   app?.use(examRoutes);
 };
