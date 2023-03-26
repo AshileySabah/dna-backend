1ff545108d8d7599be67c2024cc5a091b09e2dbc
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-25 23:06:01 -0300

    fix: cors allow origin

--- api/config/cors.js
+++ api/config/cors.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
-  res?.setHeader("Access-Control-Allow-Origin", req?.header("Origin"));
+  res?.setHeader("Access-Control-Allow-Origin", "*");
   res?.removeHeader("x-powered-by");
-  res?.setHeader("Access-Control-Allow-Methods", req?.method);
+  res?.setHeader("Access-Control-Allow-Methods", ["GET", "POST"]);
   res?.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 };
