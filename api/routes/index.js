41c4e2ca35b010d75e31f5305e858cf494a6de16
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-26 15:42:37 -0300

    feat: header config cors

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,17 @@ const cors = require("cors");
 
 module.exports = (app) => {
   app?.use(bodyParser.json());
-  app.use(cors());
+  // app.use(cors());
+
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept",
+    );
+    next();
+  });
+
   app?.get("/", (req, res) =>
     res?.status(200)?.send({ message: "Welcome to DNA service" }),
   );
