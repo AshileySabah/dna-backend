d9e75393ab9cae93166adc93e64288d023c4b174
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-26 14:02:35 -0300

    fix: mysql2 issue

--- api/models/index.js
+++ api/models/index.js
@@ -8,18 +8,17 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
+const mysql2 = require("mysql2");
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config,
-  );
-}
+const sequelize = new Sequelize(
+  config?.database,
+  config?.username,
+  config?.password,
+  {
+    ...config,
+    dialectModule: mysql2,
+  },
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
