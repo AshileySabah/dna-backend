3f264d5988d7ed801666c65fe315405f0253151e
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-26 13:11:50 -0300

    feat: cors dependency

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,10 @@
 const bodyParser = require("body-parser");
 const examRoutes = require("./examRoutes");
-const cors = require("../config/cors");
+const cors = require("cors");
 
 module.exports = (app) => {
   app?.use(bodyParser.json());
-  app?.use(cors);
+  app.use(cors());
 
   app?.use(examRoutes);
 };
